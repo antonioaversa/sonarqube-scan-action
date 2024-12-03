5ac3fc026edb41b428288b32c2be96e6dc945cc4
Antonio Aversa <antonio.aversa@sonarsource.com>
2024-12-03 01:50:27 +0100

    Update run-sonar-scanner.sh with echos

--- run-sonar-scanner.sh
+++ run-sonar-scanner.sh
@@ -24,11 +24,13 @@ fi
 if [[ -n "${SONAR_ROOT_CERT}" ]]; then
   echo "Adding SSL certificate to the Scanner truststore"
   rm -f $RUNNER_TEMP/tmpcert.pem
+  echo "AAA: $RUNNER_TEMP"
   echo "${SONAR_ROOT_CERT}" > $RUNNER_TEMP/tmpcert.pem
   # Use keytool for now, as SonarQube 10.6 and below doesn't support openssl generated keystores
   # keytool require a password > 6 characters, so we wan't use the default password 'sonar'
   store_pass=changeit
   mkdir -p ~/.sonar/ssl
+  echo "AAA: " ~/.sonar/ssl
   $SONAR_SCANNER_JRE/bin/java sun.security.tools.keytool.Main -storetype PKCS12 -keystore ~/.sonar/ssl/truststore.p12 -storepass $store_pass -noprompt -trustcacerts -importcert -alias sonar -file $RUNNER_TEMP/tmpcert.pem
   scanner_args+=("-Dsonar.scanner.truststorePassword=$store_pass")
 fi
